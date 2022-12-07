d244c6e4004b52b2566385d0bd8ed55b68f24aaf
Mrityunjay Kumar Mishra <mrityunjay.mishra@quantiphi.com>
2022-12-07 18:14:43 +0530

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import "./App.css";
 import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+
+const Hatspage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
+const about = () => {
+  return (
+    <div>
+      <h1>Welcome to About</h1>
+    </div>
+  );
+};
 
 function App() {
-  return <div>
-      <HomePage />
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={Hatspage} />
+        <Route path="/about" component={about} />
+      </Switch>
     </div>
+  );
 }
 
 export default App;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import './homepage.styles.scss';
+import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
-
-
-const HomePage = () => {
-  return <div className="homepage">
-    <Directory />
-  </div>;
+const HomePage = ({ history }) => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
 };
 export default HomePage;
